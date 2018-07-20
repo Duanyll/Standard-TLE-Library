98aaa8c3fb131a32324cec1b94b86460e91b5740
duanyll <xiaoguo2004@sina.cn>
2018-07-20 16:26:10 +0800

    全体更改拓展名为hpp 加入快速读入，kmp

---
+++ KMP.hpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+const int MAXM = 1e4 + 10;
+const int MAXN = 1e6 + 10;
+
+template <typename value_t>
+struct KMP
+{
+  public:
+    //S:源串    T:模式串
+    int operator()(const value_t *S, const int n, const value_t *T, const int m)
+    {
+        getnext(T, m);
+        return search(S, n, T, m);
+    }
+
+  private:
+    int next[MAXM];
+    void getnext(const value_t *T, const int m)
+    {
+        int j = 0, k = -1; //j前缀，p后缀
+        next[0] = -1;
+        while (j < m - 1)
+        {
+            if (k == -1 || T[j] == T[k])
+            {
+                j++;
+                k++;
+                if (T[j] != T[k])
+                {
+                    next[j] = k;
+                }
+                else
+                {
+                    next[j] = next[k];
+                }
+            }
+            else
+            {
+                k = next[k];
+            }
+        }
+    }
+    int search(const value_t *S, const int n, const value_t *T, const int m)
+    {
+        int i = 0, j = 0;
+        while (i < n && j < m)
+        {
+            if (j == -1 || S[i] == T[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                j = next[j];
+            }
+        }
+        if (j == m)
+        {
+            return i - j;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+};
