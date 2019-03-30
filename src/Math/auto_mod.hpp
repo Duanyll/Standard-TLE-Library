6c8c29655443e4c2bc8b79290718d6f4d9e3e06c
Tony Duan <xiaoguo2004@sina.cn>
2019-03-30 17:25:26 +0800

    Update auto_mod.hpp

--- src/Math/auto_mod.hpp
+++ src/Math/auto_mod.hpp
@@ -1,24 +1,34 @@
+int mod;
 template<typename T>
 struct auto_mod{
-	T val;
-	static T mod;
-	auto_mod(T x){
-		val = ((x % mod) + mod) % mod;
-	}
-		
-	operator T(){
-		return val;
-	}
+    T val;
+    auto_mod(T x){
+        val = ((x % mod) + mod) % mod;
+    }
+    auto_mod(){
+    	val = 0;
+    }
+    operator T(){
+        return val;
+    }
 };
 template<typename T>
 static auto_mod<T> operator+(const auto_mod<T>& a, const auto_mod<T>& b){
-	return auto_mod<T>(a.val + b.val);
+    return auto_mod<T>(a.val + b.val);
 }
-template<typename T>	
+template<typename T>
+static auto_mod<T> operator+=(auto_mod<T>& a,const T& b){
+    return a = auto_mod<T>(a.val + b);
+}
+template<typename T>
+static auto_mod<T> operator+=(auto_mod<T>& a,const auto_mod<T>& b){
+    return a = auto_mod<T>(a.val + b.val);
+}
+template<typename T>    
 static auto_mod<T> operator-(const auto_mod<T>& a, const auto_mod<T>& b){
-	return auto_mod<T>(a.val - b.val);
+    return auto_mod<T>(a.val - b.val);
 }
-template<typename T>		
+template<typename T>        
 static auto_mod<T> operator*(const auto_mod<T>& a, const auto_mod<T>& b){
-	return auto_mod<T>(a.val * b.val);
+    return auto_mod<T>(a.val * b.val);
 }
