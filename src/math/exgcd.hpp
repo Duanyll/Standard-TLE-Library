e57812a17dde5310894ce26d6371f0c211268650
duanyll <xiaoguo2004@sina.cn>
2019-05-03 16:20:14 +0800

    模板化部分函数

--- src/math/exgcd.hpp
+++ src/math/exgcd.hpp
@@ -1,19 +1,21 @@
-int exgcd(int a, int b, int &x, int &y) {
+template <typename T>
+T exgcd(T a, T b, T &x, T &y) {
     if (!b) {
         x = 1;
         y = 0;
         return a;
     }
-    int GCD = exgcd(b, a % b, x, y);
-    int tmp = x;
+    T GCD = exgcd(b, a % b, x, y);
+    T tmp = x;
     x = y;
     y = tmp - a / b * y;
     return GCD;
 }
 
-int inv1(int a, int MOD) {
-    int x, y;
-    int d = exgcd(a, MOD, x, y);
+template <typename T>
+T inv1(T a, T MOD) {
+    T x, y;
+    T d = exgcd(a, MOD, x, y);
     if (d == 1) return (x % MOD + MOD) % MOD;
     return -1;
 }
