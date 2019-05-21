ac06aad66b068c98a22bee16a80debe29cbdf12c
Tony Duan <xiaoguo2004@sina.cn>
2019-05-21 10:17:15 +0800

    Update treap.hpp

--- src/data_structure/treap.hpp
+++ src/data_structure/treap.hpp
@@ -6,24 +6,29 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
+template <typename T, bool is_multi = false, T INF = 0x3f3f3f3f>
 class treap {
    protected:
     struct node {
         node* ch[2];
         int r, s;
         T val;
+        int count = 1; 
         //返回新节点应该放在那棵子树里
         int where(T x) {
-            if (x == val) {
-                return -1;
-            } else {
+            if(is_multi){
                 return x < val ? 0 : 1;
+            } else {
+                if (x == val) {
+                    return -1;
+                } else {
+                    return x < val ? 0 : 1;
+                }
             }
         }
 
         void mt() {
-            s = 1;
+            s = count;
             if (ch[0] != nullptr) {
                 s += ch[0]->s;
             }
@@ -69,6 +74,8 @@ class treap {
     void insert(node*& rt, T x) {
         if (rt == nullptr) {
             rt = new node(x);
+        } else if (rt->val == x) {
+        	rt->count++;
         } else {
             int d = rt->where(x);
             insert(rt->ch[d], x);
@@ -82,8 +89,10 @@ class treap {
     void remove(node*& rt, T x) {
         int d = rt->where(x);
         node* u = rt;
-        if (d != -1) {
+        if (rt->val != x) {
             remove(rt->ch[d], x);
+        } else if (rt->count > 1) {
+        	rt->count--;
         } else if (rt->ch[0] == nullptr) {
             rt = rt->ch[1];
             delete u;
@@ -100,41 +109,67 @@ class treap {
         }
     }
 
-    bool find(node* rt, T x) {
+    node* find(node* rt, T x) {
         while (rt != nullptr) {
             int d = rt->where(x);
-            if (d == -1) {
-                return true;
+            if (rt->val == x) {
+                return rt;
             } else {
                 assert(rt != rt->ch[d]);
                 rt = rt->ch[d];
             }
         }
-        return false;
+        return nullptr;
     }
 
     T kth(node* rt, int k) {
-        if (rt == nullptr || k <= 0 || k > rt->s) {
-            return 0;
+        if (rt == nullptr || k < 0 || k > rt->s) {
+            // clog << "Oops!" << endl;
+            return INF;
         }
-        int s = (rt->ch[1] == nullptr) ? 0 : rt->ch[1]->s;
-        if (k == s + 1) {
+        int s = (rt->ch[0] == nullptr) ? 0 : rt->ch[0]->s;
+        if (k >= s + 1 && k <= s + rt->count) {
             return rt->val;
         } else if (k <= s) {
-            return kth(rt->ch[1], k);
+            return kth(rt->ch[0], k);
         } else {
-            return kth(rt->ch[0], k - s - 1);
+            return kth(rt->ch[1], k - s - rt->count);
         }
     }
 
-    void mergefrom(node* src) {
-        if (src->ch[0] != nullptr) {
-            mergefrom(src->ch[0]);
+    int rank(node* rt, T x) {
+        int s = (rt->ch[0] == nullptr) ? 0 : rt->ch[0]->s;
+        if (rt->val == x) {
+            return s + 1;
+        } else if (rt->val > x) {
+            return rank(rt->ch[0], x);
+        } else {
+            return rank(rt->ch[1], x) + s + rt->count;
         }
-        if (src->ch[1] != nullptr) {
-            mergefrom(src->ch[1]);
+    }
+
+    void prev(node* rt, T x, T& result) {
+        if (rt == nullptr) {
+            return;
         }
-        insert(src->v);
+        if (rt->val < x) {
+            result = max(result, rt->val);
+            prev(rt->ch[1], x, result);
+        } else {
+            prev(rt->ch[0], x, result);
+        }
+    }
+
+    void succ(node* rt, T x, T& result) {
+        if (rt == nullptr) {
+            return;
+        }
+        if (rt->val > x) {
+            result = min(result, rt->val);
+            succ(rt->ch[0], x, result);
+        } else {
+            succ(rt->ch[1], x, result);
+        }
     }
 
    public:
@@ -152,28 +187,30 @@ class treap {
     }
 
     bool insert(T x) {
-        if (find(root, x) == false) {
-            insert(root, x);
-            cnt++;
-            return true;
-        } else {
-            return false;
+        if (!is_multi) {
+            if (find(root, x) != nullptr) {
+                return false;
+            }
         }
+        cnt++;
+        insert(root, x);
+        return true;
     }
 
-    bool remove(T x) {
-        if (find(root, x)) {
-            remove(root, x);
+    bool erase(T x) {
+        node* cur = find(root, x);
+        if (cur != nullptr) {
             cnt--;
+            remove(root, x);
             return true;
         } else {
             return false;
         }
     }
 
-    bool find(T x) { return find(root, x); }
+    bool find(T x) { return find(root, x) != nullptr; }
 
-    int count() { return cnt; }
+    int size() { return cnt; }
 
     bool empty() { return cnt == 0; }
 
@@ -188,16 +225,28 @@ class treap {
 
     T kth(int k) {
         if (k > cnt) {
-            return 0;
+            return INF;
         } else {
             return kth(root, k);
         }
     }
 
-    void mergefrom(treap src) {
-        if (src == nullptr) {
-            return;
+    int rank(T x) {
+        if (find(root, x) == nullptr) {
+            return -1;
         }
-        mergefrom(src.root);
+        return rank(root, x);
+    }
+
+    T succ(T x){
+        T result = INF;
+        succ(root, x, result);
+        return result;
+    }
+
+    T prev(T x){
+        T result = -INF;
+        prev(root, x, result);
+        return result;
     }
 };
