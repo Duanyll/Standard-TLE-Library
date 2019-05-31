b265d7e5ec514207ff3ca418762168dcb0514af1
Tony Duan <xiaoguo2004@sina.cn>
2019-05-31 20:12:05 +0800

    Update matrix.hpp

--- src/math/matrix.hpp
+++ src/math/matrix.hpp
@@ -3,21 +3,24 @@
 #include <cstring>
 #include <cstdio>
 #include <vector>
-#include <cassert>
 using namespace std;
 
 typedef long long int64;
-const int MOD = 1e9 + 7;
+const int MOD = 1e8;
 
-template <typename T>
+template <typename T, size_t h, size_t w>
 class matrix : public vector<vector<T>> {
    public:
-   	matrix(int h = 0, int w = 0, T val = 0) : vector<vector<T>>(h, vector<T>(w, val)), width(w), height(h) {}
-	int width, height;
-	matrix<T> pow(int64 p) {
-		assert(width == height);
-		matrix<T> ret = unit(width);
-		matrix<T> a(*this);
+   	matrix(T val = 0) : vector<vector<T>>(h, vector<T>(w, val)) {}
+   	matrix(const initializer_list<vector<T>>& li) : vector<vector<T>>() {
+   		for (auto i : li) {
+   			this->push_back(i);
+   		}
+   	}
+	matrix<T, h, w> pow(int64 p) {
+		static_assert(h == w, "height must equal width.");
+		matrix<T, h, w> ret = unit();
+		matrix<T, h, w> a(*this);
 		while(p > 0) {
 			if (p & 1) {
 				ret = ret * a;
@@ -27,48 +30,47 @@ class matrix : public vector<vector<T>> {
 		}
 		return ret;
 	}
-	static matrix<T> unit(int n) {
-		matrix<T> ret(n, n, 0);
-		for (int i = 0; i < n; i++) {
+	static matrix<T, h, w> unit() {
+		static_assert(h == w, "height must equal width.");
+		matrix<T, h, w> ret(0);
+		for (size_t i = 0; i < h; i++) {
 			ret[i][i] = 1;
 		}
 		return ret;
 	}
 };
 
-template <typename T>
-matrix<T> operator+(const matrix<T>& a, const matrix<T>& b) {
-	assert(a.height == b.height && a.width == b.width);
-	matrix<T> ans(a.width, a.height);
-	for (int i = 0; i < a.height; i++) {
-		for (int j = 0;j < a.width; j++) {
+template <typename T, size_t h, size_t w>
+matrix<T, h, w> operator+(const matrix<T, h, w>& a, const matrix<T, h, w>& b) {
+	matrix<T, h, w> ans;
+	for (size_t i = 0; i < h; i++) {
+		for (size_t j = 0;j < w; j++) {
 			ans[i][j] = a[i][j] + b[i][j];
-			ans[i][j] %= MOD;
+			ans[i][j] = (ans[i][j] % MOD + MOD) % MOD;
 		}
 	}
 	return ans;
 }
 
-template <typename T>
-matrix<T> operator-(const matrix<T>& a, const matrix<T>& b) {
-	assert(a.height == b.height && a.width == b.width);
-	matrix<T> ans(a.width, a.height);
-	for (int i = 0; i < a.height; i++) {
-		for (int j = 0;j < a.width; j++) {
+template <typename T, size_t h, size_t w>
+matrix<T, h, w> operator-(const matrix<T, h, w>& a, const matrix<T, h, w>& b) {
+	matrix<T, h, w> ans;
+	for (size_t i = 0; i < h; i++) {
+		for (size_t j = 0;j < w; j++) {
 			ans[i][j] = a[i][j] - b[i][j];
-			ans[i][j] %= MOD;
+			ans[i][j] = (ans[i][j] % MOD + MOD) % MOD;
 		}
 	}
 	return ans;
 }
 
-template <typename T>
-matrix<T> operator*(const matrix<T>& a, const matrix<T>& b) {
-	assert(a.width == b.height);
-	matrix<T> ans(a.height, b.width);
-	for (int i = 0; i < a.height; i++) {
-		for (int j = 0;j < b.width; j++) {
-			for (int k = 0; k < a.width; k++) {
+template <typename T, size_t h, size_t w, size_t w1>
+matrix<T, h, w> operator*(const matrix<T, h, w1>& a, const matrix<T, w1, w>& b) {
+	matrix<T, h, w> ans;
+	for (size_t k = 0; k < w1; k++) {
+		for (size_t i = 0; i < h; i++) {
+			if (a[i][k] == 0) continue;
+			for (size_t j = 0; j < w; j++) {
 				ans[i][j] += a[i][k] * b[k][j];
 				ans[i][j] %= MOD;
 			}
